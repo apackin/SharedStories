83149cc45c42ff23027adaac39092566a3dcf533
prakashmallela <prakash.mallela@gmail.com>
2016-02-17 10:28:15 -0500

    stories edit permissions

--- browser/app/story/detail/story.detail.controller.js
+++ browser/app/story/detail/story.detail.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
-app.controller('StoryDetailCtrl', function ($scope, story, users) {
+app.controller('StoryDetailCtrl', function ($scope, story, users, Auth) {
 	$scope.story = story;
 	$scope.users = users;
 	$scope.$watch('story', function () {
 		$scope.story.save();
 	}, true);
+	$scope.isAdmin = Auth.isAdmin;
 });
